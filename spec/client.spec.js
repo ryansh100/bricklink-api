455c2bd2149e3041f45ef1286ebff26bb9b4e65f
Ofir Shapira <ofir.shapira@como.com>
2022-06-09 09:18:00 +0300

    BrickLinkApiError - throwing an error object with error details

--- spec/client.spec.js
+++ spec/client.spec.js
@@ -1,14 +1,27 @@
 import test from 'ava';
-import { Client, Color } from '../src/';
-import { config } from './test.config';
+import {Client, Color} from '../src/';
+import {config} from './test.config';
+import {BrickLinkApiError} from "../src/brickLinkApiError";
 
 /** @type {Client} */
 let BL;
 test.before(() => {
-  BL = new Client(config);
+    BL = new Client(config);
 });
 
-test('Will reject if the Bricklink servers return an error code', (t) => {
-  const request = Color.get(400000000000000000);
-  return t.throwsAsync(() => BL.send(request));
+test('Will reject if the Bricklink servers return an error code',  (t) => {
+    const request = Color.get(400000000000000000);
+    return t.throwsAsync(() => BL.send(request));
+});
+
+test('Will throw BrickLinkApiError exception if the Bricklink servers return an error code', (t) => {
+    const request = Color.get(400000000000000000);
+    return t.throwsAsync(() => BL.send(request), {
+        instanceOf: BrickLinkApiError,
+        message: 'PARAMETER_MISSING_OR_INVALID'
+    }).then(error => {
+        t.is(error.meta.code, 400);
+        t.is(error.meta.description, 'Unparseable data [400000000000000000] for [color_id].');
+        t.is(error.meta.message, 'PARAMETER_MISSING_OR_INVALID');
+    });
 });
